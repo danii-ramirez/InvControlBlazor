5d7905398c0a629942d0e89e968d1d2b02ca027d
Daniel Ramirez <ramirezdani016@hotmail.com>
2024-10-27 23:39:52 -0300

    Update AuthenticationController.cs

--- InvControl/Server/Controllers/AuthenticationController.cs
+++ InvControl/Server/Controllers/AuthenticationController.cs
@@ -6,8 +6,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Text;
-using System.Timers;
-using System.Transactions;
 
 namespace InvControl.Server.Controllers
 {
