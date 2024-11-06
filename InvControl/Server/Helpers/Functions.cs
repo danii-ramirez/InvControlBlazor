c5d9e5327c876803b609d96ef184bd195b8c942c
Daniel Ramirez <ramirezdani016@hotmail.com>
2024-11-05 23:38:24 -0300

    commit

--- InvControl/Server/Helpers/Functions.cs
+++ InvControl/Server/Helpers/Functions.cs
@@ -1,4 +1,6 @@
-﻿using InvControl.Shared.Models;
+﻿using ClosedXML.Excel;
+using InvControl.Shared.Models;
+using System.Data;
 
 namespace InvControl.Server.Helpers
 {
@@ -12,6 +14,37 @@ namespace InvControl.Server.Helpers
                 permiso.Permisos = ConstruirArbolPermiso(permisos, permiso.IdPermiso);
             }
             return permisosHijos;
+        }
+
+        internal static DataTable ConvertExcelToDataTable(byte[] excelData)
+        {
+            var dataTable = new DataTable();
+            using (var stream = new MemoryStream(excelData))
+            {
+                using var workbook = new XLWorkbook(stream);
+                var worksheet = workbook.Worksheets.First();
+
+                // Agrega las columnas
+                foreach (var headerCell in worksheet.Row(1).CellsUsed())
+                {
+                    dataTable.Columns.Add(headerCell.Value.ToString());
+                }
+
+                // Agrega las filas
+                foreach (var row in worksheet.RowsUsed().Skip(1)) // Saltar la fila de encabezados
+                {
+                    var dataRow = dataTable.NewRow();
+                    int i = 0;
+
+                    foreach (var cell in row.Cells())
+                    {
+                        dataRow[i] = cell.Value;
+                        i++;
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+            return dataTable;
         }
     }
 }
---
+++ InvControl/Server/Hubs/ProcesamientoHub.cs
@@ -0,0 +1,65 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.SignalR;
+using System.Data;
+
+namespace InvControl.Server.Hubs
+{
+    public class ProcesamientoHub : Hub
+    {
+        private readonly ILogger<ProcesamientoHub> _logger;
+
+        public ProcesamientoHub(ILogger<ProcesamientoHub> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            _logger.LogInformation("Cliente conectado: {ConnectionId}", Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _logger.LogWarning("Cliente desconectado: {ConnectionId}", Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task ProcesarExcel(byte[] excelData)
+        {
+            DataTable dataTable = new();
+            using (var stream = new MemoryStream(excelData))
+            {
+                using var workbook = new XLWorkbook(stream);
+                var worksheet = workbook.Worksheets.First();
+
+                foreach (var headerCell in worksheet.Row(1).CellsUsed())
+                {
+                    dataTable.Columns.Add(headerCell.Value.ToString());
+                }
+
+                int r = 1, totalRow = worksheet.RowsUsed().Count();
+                await Clients.Caller.SendAsync("ProgresoActualizado", r, totalRow);
+
+                foreach (var row in worksheet.RowsUsed().Skip(1))
+                {
+                    int i = 0;
+                    var dataRow = dataTable.NewRow();
+
+                    foreach (var cell in row.Cells())
+                    {
+                        dataRow[i] = cell.Value;
+                        i++;
+                    }
+
+                    dataTable.Rows.Add(dataRow);
+
+                    r++;
+                    await Clients.Caller.SendAsync("ProgresoActualizado", r, totalRow);
+                }
+            }
+
+            await Clients.Caller.SendAsync("ProcesamientoCompleto", "Procesamiento terminado");
+        }
+    }
+}
